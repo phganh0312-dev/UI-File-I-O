d6fc500c1084a477c69ea71659d9db719cdce67c
ngha1610 <nguyetha16102010@gmail.com>
2026-05-25 21:06:35 +0700

    Update hashtable.py

--- datastructures/hashtable.py
+++ datastructures/hashtable.py
@@ -1,1 +1,37 @@
+# datastructures/hashtable.py
 
+class HashTable:
+
+    def __init__(self,size=100):
+
+        self.size=size
+        self.table=[[] for _ in range(size)]
+
+    def hash_function(self,key):
+
+        return hash(key)%self.size
+
+    def insert(self,key,value):
+
+        index=self.hash_function(key)
+
+        for pair in self.table[index]:
+
+            if pair[0]==key:
+
+                pair[1]=value
+                return
+
+        self.table[index].append([key,value])
+
+    def get(self,key):
+
+        index=self.hash_function(key)
+
+        for pair in self.table[index]:
+
+            if pair[0]==key:
+
+                return pair[1]
+
+        return None
