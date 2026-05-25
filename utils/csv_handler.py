facafb5e10d5894f2b36bbf7e4e31440f02a5ff6
ngha1610 <nguyetha16102010@gmail.com>
2026-05-25 21:08:10 +0700

    Update csv_handler.py

--- utils/csv_handler.py
+++ utils/csv_handler.py
@@ -1,1 +1,66 @@
+# utils/csv_handler.py
 
+import csv
+import os
+
+class CSVHandler:
+
+    @staticmethod
+    def load_csv(file):
+
+        if not os.path.exists(file):
+
+            return []
+
+        with open(
+            file,
+            'r',
+            newline='',
+            encoding='utf-8'
+        ) as f:
+
+            return list(csv.DictReader(f))
+
+
+    @staticmethod
+    def save_csv(file,data,headers):
+
+        with open(
+            file,
+            'w',
+            newline='',
+            encoding='utf-8'
+        ) as f:
+
+            writer=csv.DictWriter(
+                f,
+                fieldnames=headers
+            )
+
+            writer.writeheader()
+
+            writer.writerows(data)
+
+
+    @staticmethod
+    def append_csv(file,data,headers):
+
+        exists=os.path.exists(file)
+
+        with open(
+            file,
+            'a',
+            newline='',
+            encoding='utf-8'
+        ) as f:
+
+            writer=csv.DictWriter(
+                f,
+                fieldnames=headers
+            )
+
+            if not exists:
+
+                writer.writeheader()
+
+            writer.writerow(data)
