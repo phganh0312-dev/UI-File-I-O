87fa9349a90d0bd9e2a96fddea42c6e7473922aa
ngha1610 <nguyetha16102010@gmail.com>
2026-05-25 21:08:49 +0700

    Update auth_service.py

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,1 +1,74 @@
+# services/auth_service.py
 
+import hashlib
+
+from utils.csv_handler import CSVHandler
+
+
+USER_FILE="data/users.csv"
+
+
+class AuthService:
+
+    def register(
+        self,
+        username,
+        password,
+        role="user"
+    ):
+
+        users=CSVHandler.load_csv(USER_FILE)
+
+        for user in users:
+
+            if user["username"]==username:
+
+                return False
+
+        password=hashlib.sha256(
+            password.encode()
+        ).hexdigest()
+
+        CSVHandler.append_csv(
+
+            USER_FILE,
+
+            {
+                "username":username,
+                "password":password,
+                "role":role
+            },
+
+            [
+                "username",
+                "password",
+                "role"
+            ]
+        )
+
+        return True
+
+
+    def login(
+        self,
+        username,
+        password
+    ):
+
+        users=CSVHandler.load_csv(USER_FILE)
+
+        password=hashlib.sha256(
+            password.encode()
+        ).hexdigest()
+
+        for user in users:
+
+            if(
+                user["username"]==username
+                and
+                user["password"]==password
+            ):
+
+                return user
+
+        return None
