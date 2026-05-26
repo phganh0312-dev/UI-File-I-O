51a87218eae331279a823d77f47d436000a09440
ngha1610 <nguyetha16102010@gmail.com>
2026-05-26 12:49:43 +0700

    Update booking_service.py

--- services/booking_service.py
+++ services/booking_service.py
@@ -1,1 +1,54 @@
+# services/booking_service.py
 
+from datetime import datetime
+
+from utils.csv_handler import CSVHandler
+
+TICKET_FILE="data/tickets.csv"
+
+
+class BookingService:
+
+    def book(
+
+        self,
+        username,
+        movie,
+        seat,
+        price
+
+    ):
+
+        ticket={
+
+            "username":username,
+
+            "movie":movie,
+
+            "seat":seat,
+
+            "price":price,
+
+            "time":str(
+                datetime.now()
+            )
+
+        }
+
+        CSVHandler.append_csv(
+
+            TICKET_FILE,
+
+            ticket,
+
+            [
+                "username",
+                "movie",
+                "seat",
+                "price",
+                "time"
+            ]
+
+        )
+
+        return True
