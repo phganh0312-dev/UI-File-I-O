c542719b53c675a3ff57bfe2fce8fbd2c6a2145e
ngha1610 <nguyetha16102010@gmail.com>
2026-05-26 13:06:21 +0700

    Update history_page.py

--- interface/history_page.py
+++ interface/history_page.py
@@ -1,1 +1,53 @@
+import streamlit as st
 
+from services.booking_service import BookingService
+
+
+service = BookingService()
+
+
+def history_page():
+
+    if (
+
+        "user"
+
+        not in
+
+        st.session_state
+
+    ):
+
+        return
+
+    user = (
+
+        st.session_state
+
+        ["user"]
+
+    )
+
+    history = (
+
+        service.history(
+
+            user
+
+            ["username"]
+
+        )
+    )
+
+    st.title(
+
+        "Lịch sử vé"
+
+    )
+
+    for ticket in history:
+
+        st.write(
+
+            ticket
+        )
