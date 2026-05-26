54d33dedd51903890bd5cffd21d438f386f52cce
ngha1610 <nguyetha16102010@gmail.com>
2026-05-26 13:02:12 +0700

    Update movie_service.py

--- services/movie_service.py
+++ services/movie_service.py
@@ -1,58 +1,173 @@
-# services/movie_service.py
-
 from utils.csv_handler import CSVHandler
 
-MOVIE_FILE="data/movies.csv"
+
+MOVIE_FILE = "data/movies.csv"
 
 
 class MovieService:
 
     def get_movies(self):
 
-        return CSVHandler.load_csv(
+        movies = CSVHandler.load(
+
             MOVIE_FILE
+
         )
 
+        return sorted(
 
-    def add_movie(
+            movies,
+
+            key=lambda x:
+
+            x["title"].lower()
+
+        )
+
+    def binary_search(
 
         self,
-        movie
+
+        keyword
 
     ):
 
-        CSVHandler.append_csv(
+        movies = self.get_movies()
+
+        left = 0
+
+        right = (
+
+            len(movies)-1
+
+        )
+
+        keyword = (
+
+            keyword.lower()
+
+        )
+
+        while left <= right:
+
+            mid = (
+
+                left+right
+
+            )//2
+
+            title = (
+
+                movies[mid]
+
+                ["title"]
+
+                .lower()
+
+            )
+
+            if title == keyword:
+
+                return movies[mid]
+
+            elif title < keyword:
+
+                left = mid+1
+
+            else:
+
+                right = mid-1
+
+        return None
+
+    def get_movie(
+
+        self,
+
+        movie_id
+
+    ):
+
+        movies = (
+
+            CSVHandler.load(
+
+                MOVIE_FILE
+
+            )
+        )
+
+        for movie in movies:
+
+            if (
+
+                movie["movie_id"]
+
+                == movie_id
+
+            ):
+
+                return movie
+
+        return None
+
+    def update_revenue(
+
+        self,
+
+        movie_id,
+
+        amount
+
+    ):
+
+        movies = (
+
+            CSVHandler.load(
+
+                MOVIE_FILE
+
+            )
+        )
+
+        for movie in movies:
+
+            if (
+
+                movie["movie_id"]
+
+                == movie_id
+
+            ):
+
+                current = int(
+
+                    movie["revenue"]
+
+                )
+
+                movie["revenue"]=(
+
+                    current+amount
+
+                )
+
+        CSVHandler.overwrite(
 
             MOVIE_FILE,
 
-            movie,
+            movies,
 
             [
-                "id",
+
+                "movie_id",
+
                 "title",
+
                 "genre",
-                "duration",
-                "rating"
+
+                "revenue"
+
             ]
 
         )
-
-
-    def search_movie(
-        self,
-        keyword
-    ):
-
-        movies=self.get_movies()
-
-        return [
-
-            movie
-
-            for movie in movies
-
-            if keyword.lower()
-
-            in movie["title"].lower()
-
-        ]
