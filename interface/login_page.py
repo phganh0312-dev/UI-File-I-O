8844aac3f8ac3f05a408a4fd854da206bc4bc6c4
ngha1610 <nguyetha16102010@gmail.com>
2026-05-26 13:05:00 +0700

    Update login_page.py

--- interface/login_page.py
+++ interface/login_page.py
@@ -1,1 +1,63 @@
+import streamlit as st
 
+from services.auth_service import AuthService
+
+
+auth = AuthService()
+
+
+def login_page():
+
+    st.title("Đăng nhập")
+
+    username = st.text_input(
+
+        "Tên đăng nhập"
+
+    )
+
+    password = st.text_input(
+
+        "Mật khẩu",
+
+        type="password"
+
+    )
+
+    if st.button(
+
+        "Đăng nhập"
+
+    ):
+
+        user = auth.login(
+
+            username,
+
+            password
+
+        )
+
+        if user:
+
+            st.session_state[
+
+                "user"
+
+            ] = user
+
+            st.success(
+
+                "Đăng nhập thành công"
+
+            )
+
+            st.rerun()
+
+        else:
+
+            st.error(
+
+                "Sai thông tin"
+
+            )
